316ad482f5d159a2763bb7549f7ccb0d784f3f38
Nayan vyas <106150162+Nayan2810@users.noreply.github.com>
2023-01-22 14:46:50 +0530

    Nayan

---
+++ src/resumeData.js
@@ -0,0 +1,83 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Nayan Vyas",
+    "role": "Frontend Developer ",
+    "linkedinId":"linkedin.com/in/nayanvyas28102003",
+    "Email": "nayanvyas28102003@gmail.com",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/nayanvyas28102003/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Nayan2810",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/_haq_se_harami_",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am 2nd year student of B.Tech at Avantika University. I am currently learning Full Stack Development, Basic Python, and is a member of the Student Council of Avantika University as Operations Lead. I am passionate about technology and his interest in programming has led him to pursue a career in the field. I enjoys learning new technologies and is eager to apply them in creative projects. I has a strong desire to make a difference in the world and strives to use his skills to do so. He is a driven and ambitious individual who is always looking for new ways to push the boundaries of technology.",
+    "address":"India",
+    "website":"https://github.com/Nayan2810",
+    "education":[
+      {
+        "UniversityName":"Avantika University",
+        "specialization":"B.Tech Computer Science Engineering",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2025",
+        "Achievements":"Ncc A certificate Holder,Nss A certificate Holder"
+      },
+      
+    ],
+   
+    "skillsDescription":"Web Devlopment",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Figma"
+      },
+      {
+        "skillname":"Canva"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Project1",
+        "description":"Web application",
+        "imgurl":"images/portfolio/hotel1.jpg"
+      },
+      
+    ],
+    "testimonials":[
+      {
+        "description":"“As engineers, we were going to be in a position to change the world – not just study it.”",
+        "name":"Henry Petroski"
+      },
+      {
+        "description":"“Math is my Passion. Engineering is my Profession.”",
+        "name":"Wilfred James Dolor"
+      }
+    ]
+  }
+  
+  export default resumeData
